9e5bf69581ac0080e82928a94550263c596900c2
AttilaAKINCI <akinci.attila@gmail.com>
2021-01-27 22:53:52 +0300

    product list and product detail fragment creations

---
+++ app/src/main/java/com/akinci/moneybox/feaure/product/view/ProductListFragment.kt
@@ -0,0 +1,22 @@
+package com.akinci.moneybox.feaure.product.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.akinci.moneybox.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProductListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_product_list, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/akinci/moneybox/feaure/product/viewmodel/ProductViewModel.kt
@@ -0,0 +1,4 @@
+package com.akinci.moneybox.feaure.product.viewmodel
+
+class ProductViewModel {
+}
